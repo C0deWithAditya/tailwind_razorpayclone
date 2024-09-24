e13e3075064de6e3df0b437b78ed7260cd162ee9
Aditya Verma <adityagen.lko12@gmail.com>
2024-09-24 22:19:31 +0530

    push
    push

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Muslish: ["Mulish", "sans-serif"],
+    },
+      colors:{
+        deepcolor:"074A7E",
+        lightcolor:"#0A76C9",
+        greencolor:"#009e5c",
+        fontgrey:"#05080a",
+        darbgcolor:"#0d94fb",
+        whitebgcolor:"#f2f8fc",
+        fonthovercolor:"2428a4"
+      },
+    },
+  },
+  plugins: [],
+}
+
